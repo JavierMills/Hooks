b523a2d9d62bf11dc5580a090985755650297089
JavierMills <javier_bouncing@hotmail.com>
2022-04-17 14:08:29 -0500

    reducer

--- src/components/useReducer/reducerIntro.js
+++ src/components/useReducer/reducerIntro.js
@@ -9,11 +9,32 @@ const initiaSate = [{
 }]
 
 const ReducerTodo= (state = initiaSate, action) => {
+//QUE CUANDO EL ACTION TENGA ALGO REALICE LA ACCION SI NO QUE NO HAGA NADA
 
+    if(action?.type == 'agregar'){
+        return [...state, action.payload];
+    }
     return state;
 
 }
 
 let todos = ReducerTodo();
+
+//no hacer push cuando e utilice reducer
+
+const newTodo ={
+    id: 2,
+    todo: 'Comparar iphone',
+    done: true,
+}
+
+const AddTodoAction ={
+    type:"agregar",
+    payload: newTodo,
+}
+
+
+todos = ReducerTodo(todos, AddTodoAction);
+
 
 console.log(todos);
