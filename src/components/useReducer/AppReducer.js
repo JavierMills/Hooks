732a37ec722fa039c09ecffff20092c7aa1ffcd7
JavierMills <javier_bouncing@hotmail.com>
2022-04-25 10:33:24 -0500

    teamofer

---
+++ src/components/useReducer/AppReducer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+const AppReducer = () => {
+  return (
+    <div>
+        <h1>Reducer</h1>
+
+    </div>
+  )
+}
+
+export default AppReducer
--- src/components/useReducer/reducerIntro.js
+++ src/components/useReducer/reducerIntro.js
@@ -1,17 +1,18 @@
+import AppReducer from './AppReducer';
 
 
-
-
+<AppReducer />
 const initiaSate = [{
     id: 1,
     todo: 'Comparar iphone',
     done: false
 }]
 
-const ReducerTodo= (state = initiaSate, action) => {
+
+const ReducerTodo = (state = initiaSate, action) => {
 //QUE CUANDO EL ACTION TENGA ALGO REALICE LA ACCION SI NO QUE NO HAGA NADA
 
-    if(action?.type == 'agregar'){
+    if(action?.type === 'agregar'){
         return [...state, action.payload];
     }
     return state;
@@ -22,19 +23,35 @@ let todos = ReducerTodo();
 
 //no hacer push cuando e utilice reducer
 
+
+// creamos el nuevo todo
 const newTodo ={
     id: 2,
     todo: 'Comparar iphone',
     done: true,
 }
 
+// creamos el segundo todo
+const newTodo2 ={
+    id: 3,
+    todo: 'Comprar fruta',
+    done: true,
+}
+
+//agrgamos el nuevo todo al action
 const AddTodoAction ={
     type:"agregar",
     payload: newTodo,
 }
 
+//agrgamos el nuevo todo al action
+const addNewTodo2 ={
+    type: "agregar",
+    payload: newTodo2,
+}
 
+//invocamos la funcion y mandamos los nuevos todos
 todos = ReducerTodo(todos, AddTodoAction);
-
+todos = ReducerTodo(todos, addNewTodo2);
 
 console.log(todos);
